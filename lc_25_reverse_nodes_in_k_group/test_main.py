ff49b61b8949f0cae79557fc09d9322c578f7e97
schang1146 <sammychang2185@gmail.com>
2020-09-28 16:13:37 -0700

    progress writing test files

--- lc_25_reverse_nodes_in_k_group/test_main.py
+++ lc_25_reverse_nodes_in_k_group/test_main.py
@@ -1,12 +1,17 @@
 import unittest
-from main import *
+from main import Solution
+from main import ListNode
+
 
 class Test(unittest.TestCase):
     def setUp(self):
+        self.solution = Solution
+        self.given = [ListNode(1, ListNode(
+            2, ListNode(3, ListNode(4, ListNode(5))))), 2]
+
+    def test_given(self):
         pass
-    
-    def test_rename(self):
-        pass
+
 
 if __name__ == '__main__':
     unittest.main()
