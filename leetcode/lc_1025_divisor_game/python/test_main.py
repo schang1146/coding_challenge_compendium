c0c2b4d275b698f56fa66680f8276225fde8d80c
Sammy Chang <sammychang2185@gmail.com>
2021-02-16 12:23:54 -0800

    Add new folder and update readme

---
+++ leetcode/lc_1025_divisor_game/python/test_main.py
@@ -0,0 +1,12 @@
+import unittest
+from main import *
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        pass
+    
+    def test_rename(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
